802cc7db9cd2b6ef4555b7fed9bb1046c71032fa
denniswanjiru <denniswanjiru71@gmail.com>
2018-05-13 07:14:35 +0300

    feat[GET] Get products endpoint - Connect the app with flask-RESTful - Create ProductsList Resource - Add get all products endpoint

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
 
 app = Flask(__name__)
+api = Api(app)
+
+products = []
+
+class ProductsList(Resource):
+    def get(self):
+        return {'products': products}
+
+
+api.add_resource(ProductsList, '/products')
 
 
 if __name__ == '__main__':
